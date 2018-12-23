bd21cd3ddc741bc03f84099c17025f2815dad100
AaronRugamaGarcia <35967309+AaronRugamaGarcia@users.noreply.github.com>
2018-12-23 11:34:59 +0100

    Initial commit

---
+++ Engine_V1/fragment_core.glsl
@@ -0,0 +1,43 @@
+#version 440
+
+in vec3 vs_position;
+in vec3 vs_color;
+in vec2 vs_texcoord;
+in vec3 vs_normal;
+
+out vec4 fs_color;
+
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+
+uniform vec3 lightPos0;
+uniform vec3 cameraPos;
+
+void main()
+{
+	//fs_color = vec4(vs_color, 1.f);
+	
+	//Ambient light
+	vec3 ambientLight = vec3(0.1f, 0.1f, 0.1f);
+
+	//Diffuse light
+	vec3 posToLightDirVec = normalize(vs_position - lightPos0);
+	vec3 difusseColor = vec3(1.f, 1.f, 1.f);
+	float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
+	vec3 diffuseFinal = difusseColor * diffuse;
+
+	//Specular Light
+	vec3 lightToPosDirVec = normalize(lightPos0 - vs_position);
+	
+
+	//Attenuation
+
+
+	//Finalight
+
+	
+	fs_color = 
+	texture(texture1, vs_texcoord) * vec4(vs_color, 1.f) 
+	* (vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f));// + vec4(specularFinal, 1.f));
+
+}
